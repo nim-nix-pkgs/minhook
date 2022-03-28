88f3ed07a169a2c22b0c093d98cf67005af0b2b4
hugosenari <hugosenari@gmail.com>
2022-03-27 21:52:13 -0300

    chore: re index minhook

--- main/flake.nix
+++ main/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-minhook-main.ref   = "refs/heads/main";
   inputs.src-minhook-main.owner = "khchen";
   inputs.src-minhook-main.repo  = "minhook";
-  inputs.src-minhook-main.dir   = "";
   inputs.src-minhook-main.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
